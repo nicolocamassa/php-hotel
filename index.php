d7b66b18b9df7160c4bf283c40dc821229356409
nicolocamassa <camassanicolo03@gmail.com>
2024-01-25 15:57:00 +0100

    [UPDATE] print all hotels data in index

--- index.php
+++ index.php
@@ -1,18 +1,64 @@
-<?php 
-
+<?php
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hotel list</title>
 </head>
-<body>
 
+<body>
+    <?php 
+        foreach($hotels as $hotel){
+            foreach($hotel as $key => $value){
+                echo "<div>".$value."</div>";
+            }
+        }
+    ?>
     <!-- TODO: 1. Stampare tutte le info di $hotels -->
     <!--       2. Aggiungere i dati dell'array in una tabella
                3. BONUS -->
 </body>
+
 </html>
