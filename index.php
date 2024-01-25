dd0da0a04f42e945d13bbdf0608c3001994c2e15
nicolocamassa <camassanicolo03@gmail.com>
2024-01-25 15:51:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- TODO: 1. Stampare tutte le info di $hotels -->
+    <!--       2. Aggiungere i dati dell'array in una tabella
+               3. BONUS -->
+</body>
+</html>
